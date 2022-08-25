795ec8a5e48b24cf3f1b4f6e48a78be549d519a6
IgorSilvaJ <igorjsilvabiz@gmail.com>
2022-08-25 20:35:33 -0300

    Adiciona pasta dos exercicios do dia

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/scripts.js
@@ -0,0 +1,17 @@
+// // console.log(document.getElementById("teste").innerHTML); // retorna tudo
+// // console.log(document.getElementById("teste").innerText); // retorna só texto
+// // document.getElementById("teste").innerText = "Olá"; // altera texto total
+
+// document.getElementById("page-title").innerText = "Qualquer filme de ação";
+// document.getElementById("paragraph").style.color = "green"
+// document.getElementById("subtitle").innerText = "Um Subtítulo";
+// document.getElementById("second-paragraph").style.backgroundColor = "green"
+
+// // document.getElementsByClassName("Nomedaclasse")[0].innerText = "OK";
+// document.getElementsByClassName("primeiro")[0].style.backgroundColor = "red";
+// document.getElementsByTagName("p")[1].style.color = "white";
+
+// // console.log(document.getElementsByTagName("p")[0]);
+
+// document.querySelector("#page-title");
+// document.querySelectorAll("h2 li:nth-child(odd)");

76a70a378443162bc0cc1bfede3a5aa7ad56cccc
IgorSilvaJ <igorjsilvabiz@gmail.com>
2022-08-30 20:12:34 -0300

    Adiciona exercicios do dia

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/ParaFixar/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/ParaFixar/script.js
@@ -22,7 +22,7 @@ function adicionaTech(elemento) {
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-input.addEventListener('keyup', function(elemento) {
+input.addEventListener('keyup', function (elemento) {
     let item = document.querySelector('.tech');
     item.innerText = elemento.target.value;
 });
@@ -30,25 +30,25 @@ input.addEventListener('keyup', function(elemento) {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-myWebpage.addEventListener('dblclick', function() {
+myWebpage.addEventListener('dblclick', function () {
     window.open('https://developer.mozilla.org/en-US/docs/Web/API/Window/open', '_blank');
 });
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-myWebpage.addEventListener('mouseenter', function() {
-myWebpage.style.color = 'red';
+myWebpage.addEventListener('mouseenter', function () {
+    myWebpage.style.color = 'red';
 });
-myWebpage.addEventListener('mouseleave', function() {
+myWebpage.addEventListener('mouseleave', function () {
     myWebpage.style.color = 'white';
-    });
+});
 
 // Segue abaixo um exemplo do uso de event.target:
 function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+    // O Event é passado como um parâmetro para a função.
+    event.target.innerText = 'Opção reiniciada';
+    // O event possui várias propriedades, porém a mais usada é o event.target,
+    // que retorna o objeto que disparou o evento.
 }
 
 firstLi.addEventListener('dblclick', resetText);

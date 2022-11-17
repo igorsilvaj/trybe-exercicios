d7866fb265d1320b7843000a5bd4ebcafb853d95
IgorSilvaJ <igorjsilvabiz@gmail.com>
2022-11-17 18:37:01 -0300

    Adiciona pasta do dia

--- 02-front-end/secao-06-testes-automatizados-com-react-testing-library/Exercicios/todo-list/src/App.js
+++ 02-front-end/secao-06-testes-automatizados-com-react-testing-library/Exercicios/todo-list/src/App.js
@@ -1,49 +1,36 @@
-// App.js
 import React, { Component } from 'react';
-import './App.css';
+import InputTodo from './InputTodo';
+import Item from './Item';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      email: '',
-      saveEmail: '',
+      listTodo: [],
     };
+
+    this.addTodo = this.addTodo.bind(this);
   }
 
-  changeEmail(value) {
-    this.setState({ email: value });
-  }
-
-  changeSaveEmail(value) {
-    this.setState({ saveEmail: value, email: '' });
+  addTodo(todo) {
+    this.setState((state) => ({ listTodo: [...state.listTodo, todo] }));
   }
 
   render() {
-    const { email, saveEmail } = this.state;
+    const { listTodo } = this.state;
     return (
-      <div className="App">
-        <label htmlFor="id-email">
-          Email
-          <input
-            id="id-email"
-            value={ email }
-            type="email"
-            onChange={ (e) => this.changeEmail(e.target.value) }
-          />
-        </label>
-        <input
-          id="btn-enviar"
-          type="button"
-          data-testid="id-send"
-          value="Enviar"
-          onClick={ () => this.changeSaveEmail(email) }
-        />
-        <input id="btn-id" type="button" value="Voltar" />
-        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
-      </div>
+      <main className="App">
+        <InputTodo addTodo={ (todo) => this.addTodo(todo) } />
+        <ul>
+          { listTodo.map((todo, index) => (
+            <li key={ index + 1 }>
+              <Item content={ todo } />
+            </li>
+          )) }
+        </ul>
+      </main>
     );
   }
 }
-
 export default App;
